e42b34ecbbb6341f5e04565e985a08ce98f17e0b
Joel <joelbmolina@gmail.com>
2017-02-16 18:36:51 -0700

    update

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,15 +1,15 @@
 angular.module('whether.services', [])
 
-.factory('Location', function() {
+.factory('Location', function () {
   return {
     geolocate: function (callback) {
-      let success = function(position) {
-        callback(position.coords.latitude, position.coords.longitude);
-      };
+      let success = function (position) {
+        callback(position.coords.latitude, position.coords.longitude)
+      }
       let error = function () {
-        console.error('ERROR: Unable to retrieve location');
-      };
-      navigator.geolocation.getCurrentPosition(success, error);
+        console.error('ERROR: Unable to retrieve location')
+      }
+      navigator.geolocation.getCurrentPosition(success, error)
     }
-  };
-});
+  }
+})
--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require('express')
+const morgan = require('morgan')
 
-let app = express();
+let app = express()
 
-app.use(morgan('dev'));
-app.use(express.static(__dirname + '/../client'));
+app.use(morgan('dev'))
+app.use(express.static(__dirname + '/../client'))
 
-app.listen(8000);
+app.listen(8000)
 
-console.log('LISTENING - localhost:8000');
+console.log('LISTENING - 8K')
 
-module.exports = app;
+module.exports = app
