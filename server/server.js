fb5c00e2cc67fb707fa7d87d310f85d25bcc2c98
joel <joelbmolina@gmail.com>
2017-03-17 20:18:35 -0700

    changes concat to path join

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const path = require('path')
 
 let app = express()
 
 app.use(morgan('dev'))
-app.use(express.static(__dirname + '/../client'))
+app.use(express.static(path.join(__dirname, '/../client')))
 
 app.listen(8000)
 
